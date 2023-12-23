5b5d1e248affbea1465a51283e82d2b416aacc3b
DianaDmitriev4 <qwertyuiop25852@mail.ru>
2023-12-23 13:08:58 +0700

    no message

---
+++ Notes/Notes/CoreData/NoteEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  NoteEntity+CoreDataClass.swift
+//  Notes
+//
+//  Created by User on 22.12.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NoteEntity)
+public class NoteEntity: NSManagedObject {
+
+}
